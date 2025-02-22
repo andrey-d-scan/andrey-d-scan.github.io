dcc4a4f4c6303910a9b98dcdf1127049dd94972a
andrey_d.scan <105270213+andrey-d-scan@users.noreply.github.com>
2025-02-22 08:49:17 +0700

    Update script.js
    Update Header

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-const hamburger = document.querySelector('.hamburger'),
-    closeElem = document.querySelector('.menu__close'),    
-    closeItem = document.querySelectorAll('.menu__item'),
-    menu = document.querySelector('.menu__wrapper');
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu__wrapper');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('menu__wrapper-active');
-});
+    if (hamburger && menu) {
+        hamburger.addEventListener('click', () => {
+            menu.classList.add('menu__wrapper-active');
+        });
+    } 
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('menu__wrapper-active');
-});
-
-closeItem.forEach(item => {
-    item.addEventListener('click', () => {
-        menu.classList.remove('menu__wrapper-active');
-    })
+    if (menu) {
+        menu.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.closest('.menu__close') || target.closest('.menu__item')) {
+                menu.classList.remove('menu__wrapper-active');
+            }
+        });
+    } 
 });
